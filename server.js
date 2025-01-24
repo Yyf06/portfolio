1c0f02a3170539094fafc1e0da2112fbabc44082
Yyf06 <122849696+Yyf06@users.noreply.github.com>
2025-01-24 14:15:34 +1000

    Fix mail setup

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+
 // server used to send send emails
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(3000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
@@ -20,6 +22,8 @@ const contactEmail = nodemailer.createTransport({
   },
 });
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Provided" : "Not Provided");
 contactEmail.verify((error) => {
   if (error) {
     console.log(error);
